6ce679cf2543e1214dfd60262a6f195e9ad8dde0
Linna Zhang <lzhang0704@gmail.com>
2023-10-14 20:53:24 -0700

    question header complete

--- client/App.js
+++ client/App.js
@@ -5,10 +5,13 @@ import Prompt from './components/Prompt';
 import Answer from './components/Answer';
 
 export default function App() {
+  const question = "What made you smile today?";
+  const ansCount = "23";
+  const minute = "12";
   return (
     <View styles={styles.container}>
       <Header />
-      <Prompt />
+      <Prompt question = {question} answerCount={ansCount} minute={minute}/>
       <Answer />
     </View>
   );
--- client/components/Prompt.js
+++ client/components/Prompt.js
@@ -1,18 +1,59 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
 
-export default function Prompt() {
-  
-    return (
-      <View>
-        <Text style={styles.qHeader}>Today's Question</Text>
+export default function Prompt(props) {
+  return (
+    <View>
+      <Text style={styles.qLetter}>Q.</Text>
+      <Text style={styles.qHeader}>{props.question}</Text>
+      <View style={styles.horizontalDiv}>
+        <Text style={styles.itemStyle}>{props.answerCount} answers</Text>
+        <Text style={styles.itemStyle}>•</Text>
+        <Text style={styles.itemStyle}>{props.minute} minutes</Text>
       </View>
-    );
-};
-  
+      <View style={styles.container}>
+      <View style={styles.line}></View>
+    </View>
+    </View>
+  );
+}
+
 const styles = StyleSheet.create({
-    qHeader: {
-        color: '#ED3952',
-        fontSize: 15
-    },
+  qLetter: {
+    color: "#ED3952",
+    fontSize: 26,
+    textAlign: "center",
+    fontWeight: "500",
+  },
+  qHeader: {
+    color: "black",
+    fontSize: 25,
+    textAlign: "center",
+  },
+  horizontalDiv: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    paddingTop: 10,
+  },
+  itemStyle: {
+    fontSize: 10,
+    margin: 10,
+    opacity: 0.5,
+    fontWeight: "600",
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    paddingTop: 20,
+    paddingBottom: 20,
+  },
+  line: {
+    height: 1,
+    width: '80%',
+    backgroundColor: 'black',
+    opacity: 0.5,
+  },
 });
