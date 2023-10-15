6ea74d55604f061e27f8c40a9c288c2f2730c4c1
Joanne Lee <joannelee@Joannes-MacBook-Air.local>
2023-10-14 20:36:45 -0700

    init

--- client/components/MyBubble.js
+++ client/components/MyBubble.js
@@ -5,13 +5,47 @@ export default function MyBubble(props) {
     const { time, ans} = props;
 
     return (
-      <View>
-        <Text>{time}</Text>
-        <Text>{ans}</Text>
+      <View style={styles.container}>
+        <Text style={styles.time}>{time}</Text>
+        <View style={styles.myBubbleContainer}>
+          <View style={styles.myBubble}>
+            <Text style={styles.myBubbleText}>{ans}</Text>
+          </View>
+        </View>
       </View>
     );
 };
   
 const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    marginBottom: 50
+  },
 
+  myBubbleText: {
+    color:'white'
+  },
+
+  myBubble: {
+    width: 280,
+    padding: 16,
+    color:'white',
+    backgroundColor: '#FFC501',
+    borderRadius: 18,
+    borderBottomRightRadius: 0,
+    marginTop: 10
+  },
+
+  time: {
+    fontSize: 11,
+    color: '#888888'
+  },
+
+  myBubbleContainer: {
+    display: 'flex',
+    alignItems: 'flex-end',
+    left: 30
+  }
 });
