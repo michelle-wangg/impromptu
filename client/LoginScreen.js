51005bfb04951445cc9e0b08ec7485763295aa3a
Michelle W <michelle@Michelles-MacBook-Pro.local>
2023-10-15 02:00:54 -0700

    auth setup

--- client/LoginScreen.js
+++ client/LoginScreen.js
@@ -1,33 +1,54 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import { FIREBASE_AUTH } from './FirebaseConfig';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 
 export default function LoginScreen({ navigation }) {
-  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const auth = FIREBASE_AUTH;
 
-  const handleLogin = () => {
-    if (username === 'U' && password === 'P') {
-      navigation.navigate('Landing');
+  const handleSignUp = async () => {
+    try {
+      const response = await createUserWithEmailAndPassword(auth, email, password);
+      console.log(response);
+      
+    } catch (error) {
+      console.log(error);
+      alert('Sign up failed: ' + error.message);
     }
-  };
+};
+
+const handleLogin = async () => {
+    try {
+      const response = await signInWithEmailAndPassword(auth, email, password);
+      console.log(response);
+      alert('Logged in successfully');
+      navigation.navigate('Landing');
+    } catch (error) {
+      console.log(error);
+      alert('Login failed: ' + error.message);
+    }
+};
 
   return (
     <View style={styles.container}>
-       <Text style={styles.logo}>Im<Text style={styles.boldText}>prompt</Text>u</Text>
-      <TextInput
-        placeholder="Username"
-        value={username}
-        onChangeText={setUsername}
-        style={styles.input}
-      />
-      <TextInput
-        placeholder="Password"
-        value={password}
-        onChangeText={setPassword}
-        secureTextEntry
-        style={styles.input}
-      />
+        <Text style={styles.logo}>Im<Text style={styles.boldText}>prompt</Text>u</Text>
+       <TextInput
+         placeholder="Emai"
+         value={email}
+         onChangeText={setEmail}
+         style={styles.input}
+       />
+       <TextInput
+         placeholder="Password"
+         value={password}
+         onChangeText={setPassword}
+         secureTextEntry
+         style={styles.input}
+       />
       <Button title="Login" onPress={handleLogin} />
+      <Button title="Sign Up" onPress={handleSignUp} />
     </View>
   );
 }
